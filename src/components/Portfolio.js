16b994403837399d2a31a8ba256cc5ca929a51a1
praveenraj027 <praveenrajak0506@gmail.com>
2025-02-21 21:52:40 +0530

    lottie added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,33 @@
-// import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import './portfolio.css';
+import lottie from "lottie-web";
 // import praveen from './praveen.jpg';
 
 const Portfolio = () => {
 
+  const lottieRef = React.useRef(null);
+  useEffect(() => {
+    fetch('https://lottie.host/a20a6d70-7fcf-4e70-9a79-f8738353a58b/UkjBzxLVJV.json')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Lottie JSON file not found: ${'https://lottie.host/a7fdaf42-64a7-4195-a375-3723038d143e/BtF5QQqjaN.json'}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        lottie.loadAnimation({
+          container: lottieRef.current,
+          renderer: "svg",
+          loop: true,
+          autoplay: true,
+          animationData: data, // Use fetched JSON data
+        });
+      })
+      .catch((error) => console.error("Error loading Lottie animation:", error));
+
+    return () => lottie.destroy();
+  }, []);
   return (
     <div className="min-h-screen text-white " id="home">
       {/* Main Content */}
@@ -50,10 +73,13 @@ const Portfolio = () => {
               I'm Praveen Rajak, a passionate Computer Science Engineering student specializing in AI and Machine Learning. I'm also an aspiring Frontend Developer with experience in HTML, CSS, JavaScript, and React.js.
             </motion.p>
           </div>
-          <div className="relative flex justify-center items-center w-1/2">
+          <div className="relative flex justify-center items-center w-1/2" >
             {/* <img src={praveen} className="rounded-3xl h-96 w-72 z-10" alt="profile" />
             <div className="absolute bg-white h-96 w-72 rounded-3xl z-20 opacity-40 flex justify-center items-center text-black">
             </div> */}
+            <div className="h-[20rem]" ref={lottieRef}>
+
+            </div>
           </div>
 
         </div>
